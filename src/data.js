688a19e0c75574b4724ae03ca2c67a9163173fb5
Kashish Goyal <kashishgoyal0003@gmail.com>
2026-03-12 20:43:44 +0530

    basic 1

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const  data=[
+  {
+    "id": 1,
+    "name": "Agra",
+    "info": "Home to the iconic Taj Mahal, Agra is a historic city rich in Mughal architecture and culture.",
+    "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQgdZaFuNEjoETwo2VE6EwqTFt5TYv4hrxRgmT6bI5-TbcB6RTjhKwcrjg_Sw_mj6v_DfvG4MgEMyEF2uAlSpYzd6w&s=19",
+    "price": "$2500"
+  },
+  {
+    "id": 2,
+    "name": "Jaipur",
+    "info": "Known as the Pink City, Jaipur is famous for its palaces, forts, and vibrant bazaars.",
+    "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTCgy1pqlzWIn1Dm2DUTrQE3Uv8Dm79tP9D_Kg4h4osl9BgXOUAbYCbwoAs3NdXxarlGz7IZhS4U3Zxp9FttK26AZQ&s=19",
+    "price": "$3000"
+  },
+  {
+    "id": 3,
+    "name": "Goa",
+    "info": "A coastal paradise with golden beaches, Portuguese heritage, and lively nightlife.",
+    "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcReCUAnNyTh2LucdzhPYHhUd7voZVxNLKjF8sgFBFZHp7JlhovWiH9c2zQsRoppHqcKGnHoJ3z7Oe_eLXnR6z8mxhc&s=19",
+    "price": "$4000"
+  },
+  {
+    "id": 4,
+    "name": "Hyderabad",
+    "info": "A bustling metropolis known for the Charminar, biryani, and its blend of tradition and modernity.",
+    "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQ84kB9IYTgwRVG8YOBeP6NOK1F3d1VGpNgMAaC3LyynCY_r1ffqXZYTbaffeXn_oghxMBbHkyE7TkEPvh8AdShCXQ&s=19",
+    "price": "$3500"
+  },
+  {
+    "id": 5,
+    "name": "Visakhapatnam",
+    "info": "A scenic coastal city with beaches, hills, and a growing industrial hub.",
+    "image": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTjugk0xr1xgPSJSH76hPgTlFwMvKReDjEBy-XS8c9aT2IBAcvfXkjZ0fwUUTr3nYfhVBpkK_Vrzxu-_DF4IWSjWxs&s=19",
+    "price": "$2800"
+  }
+]
